010edb53ddf68571f6f9d7c999709e97c8e8ceb1
Ithamar R. Adema <ithamar.adema@team-embedded.nl>
2011-07-18 23:24:14 +0200

    tools: Initial commit.
    Code taken from rkutils-20100903.tar.gz, courtesy of FUKAUMI Naoki,
    aka naobsd aka fun_.
    
    Added Android.mk to build them as part of the Android build.

---
+++ Android.mk
@@ -0,0 +1,45 @@
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rkcrc.c
+
+LOCAL_MODULE := rkcrc
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_HOST_EXECUTABLE)
+
+$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rkpack.c
+
+LOCAL_MODULE := rkpack
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_HOST_EXECUTABLE)
+
+$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rkunpack.c
+
+LOCAL_MODULE := rkunpack
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_HOST_EXECUTABLE)
+
+$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rkflash.c
+
+LOCAL_MODULE := rkflash
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_HOST_EXECUTABLE)
+
+$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
