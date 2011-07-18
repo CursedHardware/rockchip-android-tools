11cba354e015042d86a913e94ed1eb5eeca63920
Ithamar R. Adema <ithamar.adema@team-embedded.nl>
2011-07-18 23:36:03 +0200

    add my afptool source.
    This is a reimplementation of the original, with identical parameter syntax
    and identical output. Might be extended once done.

--- Android.mk
+++ Android.mk
@@ -43,3 +43,14 @@ LOCAL_MODULE_TAGS := eng
 include $(BUILD_HOST_EXECUTABLE)
 
 $(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := afptool.c
+
+LOCAL_MODULE := afptool
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_HOST_EXECUTABLE)
+
+$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
